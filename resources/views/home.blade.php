0beddf71c6c8d0f97fcdbc4330d574f42acf0e16
Lucas Abrunhoza Scarpa <lucasabrunhoza@gmail.com>
2018-09-17 17:10:54 -0300

    ..

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,8 +8,17 @@
 </div>
 
 <div class="row">
-    <div class="col-xl-4 col-lg-12">
-        <div class="card card-chart">TESTE</div>
+	<div class="col-xl-3 col-lg-6 col-md-6 col-sm-6">
+		<a href='#'>
+		<div class="card card-stats">
+			<div class="card-header card-header-warning card-header-icon">
+				<div class="card-icon">
+				<i class="material-icons">content_copy</i>
+			</div>
+			<h3 class="card-title">Saladas</h3>
+        </div>
+      </div>
+  </a>
     </div>
 </div>
 
